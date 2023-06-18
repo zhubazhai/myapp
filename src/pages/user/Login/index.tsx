30961b35b9db2ed755eaa78efc59fad9cfae69f6
ex_lingfeng.meng <ex_lingfeng.meng@tcl.com>
2023-06-18 15:05:58 +0800

    fix: 解决报错

--- src/pages/user/Login/index.tsx
+++ src/pages/user/Login/index.tsx
@@ -252,7 +252,7 @@ const Login: React.FC = () => {
                   const result = await getFakeCaptcha({
                     phone,
                   });
-                  if (result === false) {
+                  if (!result) {
                     return;
                   }
                   message.success('获取验证码成功！验证码为：1234');
